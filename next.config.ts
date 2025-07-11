7e3acd16563b48312a9d5b328c7544f7cab5686c
linhngocdao <linhdaongoc22@gmail.com>
2025-07-11 19:43:28 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'cms.junglebosstours.com',
+        hostname: 'cms.junglebosstours.com,',
         port: '',
         pathname: '/assets/**',
       }
