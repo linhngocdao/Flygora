fde6319a4f617b5153e29e8f10d9f324f9744a05
linhngocdao <linhdaongoc22@gmail.com>
2025-09-24 16:02:45 +0700

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+   <div className="flex items-center justify-center min-h-screen bg-gray-50 px-4">
+      <h1 className="text-center text-4xl sm:text-6xl font-bold leading-relaxed text-gray-800">
+        Chúng tôi đang trong quá trình cập nhật website. <br />
+        Xin liên hệ để biết thêm chi tiết:{" "}
+        <span className="text-blue-600">0 793 946 789</span>
+      </h1>
+    </div>
+  );
+}
